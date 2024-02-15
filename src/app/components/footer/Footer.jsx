59aa4d74f5ba21678e6c21c97d560886ef37812c
ozanyusufoglu <ozanyusufoglu@gmail.com>
2024-02-15 20:08:33 +0300

    footer updated

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import styles from './footer.module.css'
-
+import Link from 'next/link'
+import Image from 'next/image'
 export default function Footer() {
-
-    const date = new Date()
 
     return (
         <div className={styles.component}>
-            <p>Built by Ozan with 🐵🧠</p>
+            <div >
+
+                <Link href="https://github.com/ozanyusufoglu/scrimba-nextjs-workshop"
+                    className={styles.sourceCode}>
+                    <Image src="github-mark.svg"
+                        alt="github logo"
+                        width={30}
+                        height={30}></Image>
+                    <p>Show me to the source code</p>
+                </Link>
+            </div>
+
+            <br />
+            <br />
+            <div>
+                <p>Built by <Link href="https://github.com/ozanyusufoglu/">Ozan</Link> with 🐵🧠</p>
+            </div>
             <br />
             <h4>2024</h4>
-            <br />
-
         </div>
     )
 }
